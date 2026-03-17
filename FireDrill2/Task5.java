74e23c3cd92059d1e68c3980f419ca25103927e2
Your Name <your-github-email@example.com>
2026-03-17 16:02:35 +0100

    firedrill 2 task 6

---
+++ FireDrill2/Task5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        Scanner inputReader = new Scanner(System.in);
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Enter score : ");
+            int score = inputReader.nextInt();
+            if (score % 2 == 0){
+            sum += score; 
+            }
+           }
+            System.out.println("The sum of even scores :" +sum);
+         
+            }
+}
+
---
+++ FireDrill2/Task6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Task6 {
+    public static void main(String[] args) {
+    	Scanner InputReader= new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        
+        for (int i = 1; i <= 10; i++) {
+        System.out.println("Enter a number:");
+       int score = InputReader.nextInt();
+      
+        if (score % 2 == 0){
+            sum += score; 
+            count++;
+            }
+           }
+         
+           	double average = (double)sum/count;
+            System.out.println("The average of the score :" +average );
+            }
+}
+
