4252b8d3cd952d4e81e8762bbf27dbd8d331dca4
Your Name <your-github-email@example.com>
2026-03-17 14:31:07 +0100

    Fire Drill 2 Task 2

---
+++ FireDrill2/Task2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner inputReader = new Scanner(System.in);
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Enter score " + i + ": ");
+            int score = inputReader.nextInt();
+            sum+= score; 
+           }
+            double average = sum / 10.0;
+            System.out.println("The average is:" +average);
+         
+            }
+}
