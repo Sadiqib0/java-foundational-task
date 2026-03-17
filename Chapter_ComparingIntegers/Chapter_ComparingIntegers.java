b8b4d2094c131b78f622830a06d3f57873ab01c1
Your Name <your-github-email@example.com>
2026-03-17 06:49:17 +0100

    Added Comparing Integers 2.14

---
+++ Chapter_ComparingIntegers/Chapter_ComparingIntegers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ComparingIntegers {
+    public static void main(String[] args) {
+        Scanner inputReader = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int number1 = inputReader.nextInt(); 
+
+        System.out.print("Enter second integer: ");
+        int number2 = inputReader.nextInt(); 
+
+        if (number1 > number2) {
+            System.out.printf("%d is larger%n", number1);
+        }
+
+        if (number2 > number1) {
+            System.out.printf("%d is larger%n", number2);
+        }
+
+        if (number1 == number2) {
+            System.out.println("These numbers are equal");
+        }
+    } 
+} 
