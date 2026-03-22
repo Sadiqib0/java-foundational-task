73d2c245764379deff012c73aaa05cf696a49141
Your Name <your-github-email@example.com>
2026-03-22 23:14:31 +0100

    Added Arraykata Code

--- src/weekend1/test/BtsCalculatorTest.java
+++ src/weekend1/test/BtsCalculatorTest.java
@@ -4,50 +4,50 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BtsCalculatorTest {
 
     @Test
-     void lessThan_50_Delivery() {
+     void lessThan_50_Delivery_shouldUse160PerParcel() {
         int wage = BtsCalculator.calculateWage(25);
         assertEquals(9000, wage);
     }
 
     @Test
-    void exactly_50_Deliveries() {
+    void exactly_50_Deliveries_shouldUse200PerParcel() {
         int wage = BtsCalculator.calculateWage(50);
         assertEquals(15000, wage);
     }
 
     @Test
-    void exactly_59_Deliveries() {
+    void exactly_59_Deliveries_() {
         int wage = BtsCalculator.calculateWage(59);
         assertEquals(16800, wage);
     }
 
     @Test
-    void exactly_60_Deliveries() {
+    void exactly_60_Deliveries_shouldUse250PerParcel() {
         int wage = BtsCalculator.calculateWage(60);
         assertEquals(20000, wage);
     }
 
     @Test
-    void exactly_69_Deliveries() {
+    void exactly_69_Deliveries_shouldStillBeInThe60to69Range() {
         int wage = BtsCalculator.calculateWage(69);
         assertEquals(22250, wage);
     }
 
     @Test
-    void exactly_70_Deliveries() {
+    void exactly_70_Deliveries_shouldUse500PerParcel_shouldUse500PerParcel() {
         int wage = BtsCalculator.calculateWage(70);
         assertEquals(40000, wage);
     }
 
 
     @Test
-    void exactly_100_Deliveries() {
+    void exactly_100_Deliveries_shouldUse500PerParcel() {
         int wage = BtsCalculator.calculateWage(100);
         assertEquals(55000, wage);
     }
 
     @Test
-    void negativeDeliveriesNumbers() {
+    void negativeDeliveries_shouldThrowIllegalArgumentException() {
         IllegalArgumentException exception = assertThrows(
                 IllegalArgumentException.class,
                 () -> BtsCalculator.calculateWage(-1)
